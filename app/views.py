8d52d209b97195b96455133437d6fa55dd6acd7d
sjsghli <sjs@mailbolt.com>
2026-03-11 12:47:49 +0000

    s

--- app/views.py
+++ app/views.py
@@ -5,11 +5,24 @@ import requests
 def index(request):
     if request.method == "POST":
         api_url = request.POST.get("api_url")
-        response = requests.get(api_url)
-        data = response.json()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'application/json',
+        }
+        try:
+            response = requests.get(api_url, headers=headers)
         
-       
-        context = {"data": data}
+            code = response.status_code
+            text = response.text
+            # repr() will show if it's truly empty
+            data = response.json()
+        
+        except requests.exceptions.RequestException as e:
+            code = 500
+            text = str(e)
+            data = {}
+        
+        context = {"data": data, "code": code, "text": text}
         return render(request, "app/api.html", context)
         
     return render(request, "app/index.html")
